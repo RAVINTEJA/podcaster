447e009a4c5c7835f11f433c0bdd6e701170325a
RAVINTEJA <ravinteja.chilukamari@gmail.com>
2023-07-28 23:40:09 +0530

    Initialxed the app and installed required librarires

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/regular.min.css" integrity="sha512-WidMaWaNmZqjk3gDE6KBFCoDpBz9stTsTZZTeocfq/eDNkLfpakEd7qR0bPejvy/x0iT0dvzIq4IirnBtVer5A==" />
+      </Head>
+      <body >
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#9f74eb",
+        secondary: "#f6f2ff",
+        text: "#2b2c2b",
+        background: "#ffffff",
+      }
+    }
+
+  },
+}
